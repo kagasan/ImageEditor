e2987d6636da7b223fac900f96016d2ab6502826
kagasan <ghghkaja@gmail.com>
2017-12-07 14:45:33 +0900

    canvas

--- imageeditor.js
+++ imageeditor.js
@@ -1,1 +1,16 @@
 //imageeditor.js
+
+var canvas;
+var ctx;
+
+window.onload = function(){
+    canvas = document.getElementById("canvas");
+    ctx = canvas.getContext('2d');
+    canvas.height = 480;
+    canvas.width = 640;
+    ctx.fillStyle="#999999";
+    ctx.fillRect(0, 0, 640, 480);
+    ctx.font = "16px 'メイリオ'";
+    ctx.fillStyle="#0000ff";
+    ctx.fillText("ここに画像をドラッグアンドドロップ", 0, 16);
+}
